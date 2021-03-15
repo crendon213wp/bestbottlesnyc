c2512b5c547e970eae88822b381763dfc36223ed
crendon213wp <ciscorendon213@gmail.com>
2021-03-14 23:09:25 -0400

    initial commit

---
+++ js/apps.js
@@ -0,0 +1,11 @@
+const newApp = new Vue ({
+    el: '#helloApp',
+    data: {
+        title:"this is a test website",
+        message:"Lorem ipsum dolor, sit amet consectetur adipisicing elit. Impedit commodi quibusdam, reiciendis dolores deleniti est numquam aperiam totam facere provident omnis eligendi sunt maiores nam. Dignissimos iusto explicabo sed eaque at odio assumenda quisquam, inventore culpa provident eligendi perferendis consequatur ipsam quos obcaecati asperiores magni repellat sit, excepturi deleniti repellendus dolorum dolores temporibus dicta. Dolore dolorum nobis voluptatum officiis, possimus sit accusantium doloribus, veniam exercitationem aspernatur architecto eligendi rerum deleniti animi sapiente perspiciatis, expedita temporibus tempore nam neque totam id? Nulla, nesciunt. Voluptas atque illo illum? Doloremque officiis molestias a iste saepe magni beatae in repellendus, reiciendis vero illum minus!"
+    }
+});
+
+
+
+// $('.carousel-1').prepend('<img id="carousel-1" src="assets/wine-glasses.jpg" />');
